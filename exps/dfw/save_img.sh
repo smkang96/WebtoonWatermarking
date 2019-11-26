cca4787b3bf874085d105f37b447272b28cfc043
Haritz Puerto-San-Roman <haritzpuerto94@gmail.com>
2019-11-26 23:02:06 +0900

    Added noises to show images, but not working yet

--- exps/dfw/save_img.sh
+++ exps/dfw/save_img.sh
@@ -1,2 +1,17 @@
 python $CS548_DIR/dfw/run.py save_img \
-  --img_size 256 --msg_l 31 --device 0 --noise_type no_noise --n_imgs 10
+  --img_size 256 --msg_l 26 --device 0 --noise_type no_noise --n_imgs 10
+
+python $CS548_DIR/dfw/run.py save_img \
+  --img_size 256 --msg_l 26 --device 0 --noise_type crop --n_imgs 10
+
+python $CS548_DIR/dfw/run.py save_img \
+  --img_size 256 --msg_l 26 --device 0 --noise_type cropout --n_imgs 10
+
+python $CS548_DIR/dfw/run.py save_img \
+  --img_size 256 --msg_l 26 --device 0 --noise_type dropout --n_imgs 10
+
+python $CS548_DIR/dfw/run.py save_img \
+  --img_size 256 --msg_l 26 --device 0 --noise_type resize --n_imgs 10
+
+python $CS548_DIR/dfw/run.py save_img \
+  --img_size 256 --msg_l 26 --device 0 --noise_type jpeg --n_imgs 10
